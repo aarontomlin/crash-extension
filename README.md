f5667ca9e4a521c0aaa31303fb74c169ac0b0efd
Aaron Tomlin <atomlin@redhat.com>
2013-01-28 10:53:36 +0000

    swap usage: Extended to work with RHEL 5, 6 and 7.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 crash-extension
 ===============
+Compilation notes:
+gcc -nostartfiles -shared -rdynamic -o swap_usage.so swap_usage.c -fPIC -DX86_64
--- swap_usage.c
+++ swap_usage.c
@@ -1,8 +1,8 @@
-/* swap-usage.c - Check swap usage for each process
+/* swap-usage.c - Check actual swap consumption for each process
  *
- * Copyright (C) 2001, 2002 Mission Critical Linux, Inc.
- * Copyright (C) 2002, 2003, 2004, 2005 David Anderson
- * Copyright (C) 2002, 2003, 2004, 2005 Red Hat, Inc. All rights reserved.
+ * Aaron Tomlin <atomlin@redhat.com>
+ *
+ * Copyright (C) 2013 Red Hat, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,8 @@
  */
 
 #include "defs.h"
+#define MEMBER_FOUND 1
+#define MEMBER_NOT_FOUND 0
 
 int _init(void);
 int _fini(void);
@@ -43,16 +45,63 @@ _fini(void)
 }
 
 void
-show_swap_usage(struct task_context *tc) 
+show_swap_usage(struct task_context *tc, unsigned int exists) 
 {
 	struct task_mem_usage task_mem_usage, *tm;
 	tm = &task_mem_usage;
 	get_task_mem_usage(tc->task, tm);
-	ulong swap_usage;
+	physaddr_t paddr;
+	ulong mm;
+	ulong vma;
+	ulong vm_start;
+	ulong vm_end;
+	ulong vm_next;
+	ulong swap_usage = 0;
 
-	readmem((tm->mm_struct_addr + swap_usage_offset), KVADDR, &swap_usage, sizeof(void *), "mm_counter_t", QUIET|RETURN_ON_ERROR);
-        fprintf(fp, "%5ld  %5ld  %5s\n",
-		tc->pid, swap_usage  << (PAGESHIFT()-10), tc->comm);
+	readmem(tc->task + OFFSET(task_struct_mm), KVADDR,
+        &mm, sizeof(void *), "mm_struct mm", FAULT_ON_ERROR);
+
+	if (!mm)
+		return;
+
+	switch (exists) {
+	case MEMBER_FOUND:
+
+		readmem((mm + swap_usage_offset), KVADDR, 
+		&swap_usage, sizeof(void *), "mm_counter_t", FAULT_ON_ERROR);
+
+		break;
+
+	case MEMBER_NOT_FOUND:
+	default:
+
+		readmem(mm + OFFSET(mm_struct_mmap), KVADDR,
+		&vma, sizeof(void *), "mm_struct mmap", FAULT_ON_ERROR);
+
+		for (; vma; vma = vm_next) {
+
+			readmem(vma + OFFSET(vm_area_struct_vm_start), KVADDR, 
+			&vm_start, sizeof(void *), "vm_area_struct vm_start", FAULT_ON_ERROR);
+
+			readmem(vma + OFFSET(vm_area_struct_vm_end), KVADDR,
+			&vm_end, sizeof(void *), "vm_area_struct vm_end", FAULT_ON_ERROR);
+
+			readmem(vma + OFFSET(vm_area_struct_vm_next), KVADDR,
+			&vm_next, sizeof(void *), "vm_area_struct vm_next", FAULT_ON_ERROR);
+
+			while (vm_start < vm_end) {
+				if (!uvtop(tc, vm_start, &paddr, 0)) {
+
+					if (paddr && !(paddr & _PAGE_FILE)) {
+						swap_usage++;
+					}
+				}
+				vm_start += PAGESIZE();
+			}
+		}
+	}
+	fprintf(fp, "%5ld  %5ld\t%s\n",
+	tc->pid, swap_usage  << (PAGESHIFT()-10), tc->comm);
 }
 
 
@@ -61,22 +110,19 @@ cmd_swap_usage(void)
 {
 	struct task_context *tc;
 	int i;
+	unsigned int exists = MEMBER_NOT_FOUND;
 
 	if (MEMBER_EXISTS("mm_struct", "_swap_usage")) {
 		swap_usage_offset = MEMBER_OFFSET("mm_struct", "_swap_usage");
+		exists = MEMBER_FOUND;
+	}
 
-		fprintf(fp,
-		    "PID     SWAP     COMM\n");
+	fprintf(fp,
+	    "PID     SWAP     COMM\n");
 
-		tc = FIRST_CONTEXT();
-		for (i = 0; i < RUNNING_TASKS(); i++, tc++) {
-			if (!is_kernel_thread(tc->task))
-				show_swap_usage(tc);
-		}
-
-		fprintf(fp, "\n");
-
-	} else {
-		fprintf(fp, "This extension only works under RHEL 6\n");
+	tc = FIRST_CONTEXT();
+	for (i = 0; i < RUNNING_TASKS(); i++, tc++) {
+		if (!is_kernel_thread(tc->task))
+			show_swap_usage(tc, exists);
 	}
 }
